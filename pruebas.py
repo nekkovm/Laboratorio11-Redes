7549ca170fed4e1197d1c00ca87b3b00aaf4d7dc
JorgeIsur <ramirezisur@gmail.com>
2022-12-06 00:03:02 -0600

    test file

---
+++ pruebas.py
@@ -0,0 +1,12 @@
+from modulos import crc
+from modulos import divisionMod2
+from bitarray import bitarray
+#CRC-6 1000011
+
+msg,crc_code = crc.compute("hola.txt",bitarray('1000011'),5)
+print(f'CRC of text file "hola.txt" equals {crc_code.to01()}')
+"""
+TEST BENCH
+residuo = divisionMod2.mod2div(bitarray('1110010101000'),bitarray('1101'))
+print(f"CRC:{residuo}")
+"""
